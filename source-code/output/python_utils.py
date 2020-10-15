8ec264e1d455325f0fdafe9f6fefefac1d9eff86
Yolan Uyttenhove <yuyttenhove@gmail.com>
2020-10-15 13:06:08 +0200

    python code

--- source-code/output/python_utils.py
+++ source-code/output/python_utils.py
@@ -1,10 +1,12 @@
 import cv2
 import numpy as np
+from pathlib import Path
 
 def txt_to_image(filename, savename):
     data = np.loadtxt(filename)
-    cv2.imwrite(savename, data*255)
+    cv2.imwrite(str(savename), data*255)
 
 if __name__ == "__main__":
-    fname = "result.txt"
-    txt_to_image(fname, "result.jpg")
+    path = Path(__file__).parent
+    fname = path / "result.txt"
+    txt_to_image(fname, path / "result.jpg")
