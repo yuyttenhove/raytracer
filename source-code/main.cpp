220c28eb08d7b74888cca26d9d73c8e01c321332
Yolan Uyttenhove <yuyttenhove@gmail.com>
2020-10-19 15:31:29 +0200

    add hdri

--- source-code/main.cpp
+++ source-code/main.cpp
@@ -27,26 +27,29 @@ int main() {
 
 
     // world
-    string hdri_filename = "/home/yolan/dev/raytracer/source-code/World/sunflowers_4k.png";
+    string hdri_filename = "/home/yolan/dev/raytracer/source-code/World/sunflowers_4k_light.png";
     WorldHdri world = WorldHdri(hdri_filename);
 
-//    // light
-//    EmissiveMaterial emissiveMaterial = EmissiveMaterial(1.0);
-//    Mesh emissiveMesh = Mesh(&emissiveMaterial);
-//    Triangle triangle2 = Triangle(&emissiveMesh, {1, -1, 1}, {1, 5, 3}, {1, -1, 5});
-//    emissiveMesh.addTriangle(&triangle2);
-//
-//    // floor
-//    DiffuseMaterial diffuseMaterial = DiffuseMaterial(.75);
-//    Mesh diffuseMesh = Mesh(&diffuseMaterial);
-//    Triangle diffuseTriangle = Triangle(&diffuseMesh, {-100, 0, -1}, {100, 100, -1}, {100, 100, -1});
-//    diffuseMesh.addTriangle(&diffuseTriangle);
-//
-//
-//    vector<Mesh *> meshes{&emissiveMesh, &diffuseMesh};
-//    MeshCollection meshCollection = MeshCollection(meshes);
+    // light
+    EmissiveMaterial emissiveMaterial = EmissiveMaterial(1.0);
+    Mesh emissiveMesh = Mesh(&emissiveMaterial);
+    Triangle triangle2 = Triangle(&emissiveMesh, {1, -1, 1}, {1, 5, 3}, {1, -1, 5});
+    emissiveMesh.addTriangle(&triangle2);
 
-    vector<Mesh *> meshes{};
+    // floor
+    DiffuseMaterial diffuseMaterial = DiffuseMaterial(.75);
+    Mesh diffuseMesh = Mesh(&diffuseMaterial);
+    Triangle diffuseTriangle = Triangle(&diffuseMesh, {-100, 0, -1}, {100, -100, -1}, {100, 100, -1});
+    diffuseMesh.addTriangle(&diffuseTriangle);
+
+    // object
+    DiffuseMaterial diffuseMaterial2 = DiffuseMaterial(.95);
+    Mesh diffuseMesh2 = Mesh(&diffuseMaterial2);
+    Triangle diffuseTriangle2 = Triangle(&diffuseMesh2, {3, -1, 1}, {2, -1, -1}, {3, -1, -1});
+    diffuseMesh.addTriangle(&diffuseTriangle2);
+
+
+    vector<Mesh *> meshes{&emissiveMesh, &diffuseMesh};
     MeshCollection meshCollection = MeshCollection(meshes);
 
     Scene scene = Scene(&meshCollection, &world, width, height, numberOfRaysPerBounce, bounceDepth);
